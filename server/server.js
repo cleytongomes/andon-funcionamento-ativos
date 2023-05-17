49c7741a6c5da446a568299dd200e591891ea5a8
Cleyton Gomes <cleytongomesx@gmail.com>
2023-05-17 17:15:49 -0300

    Melhora de desempenho ao se increver em tópicos

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const socketsTopics = {};
+
 const io = require("socket.io")(server, {
     cors: {
         origin: "http://localhost:3002",
@@ -26,20 +28,41 @@ io.use((socket, next) => {
 });
 
 io.on('connection', (socket) => {
+    // todas as funções do socket em uma lista?
+    // são:  on, emit, join, leave, disconnect,  broadcast, to, in, 
+    // 
+    socket.on('event', (data) => {
+        console.log(data);
+     });
     console.log('a user connected');
+    
+    socket.on('subscribe', (topic) => {
+        console.log("SUB TOPIC : " + topic);
+        client.subscribe(topic);
 
-    // socket.on('subscribe', (topic) => {
+        if (!socketsTopics[socket.id]) {
+            socketsTopics[socket.id] = new Set();
+        } 
 
-    //     console.log("LENDO DAQUI -> " + topic);
+        socketsTopics[socket.id].add(topic);
 
-    //     // const [machine] = topic.split('/');
-
-    //     // if (allowedMachines[decodedToken.username].includes(machine)) {
-    //     //     socket.join(topic);
-    //     // } else {
-    //     //     socket.emit('error', 'Você não tem permissão para acessar essa máquina.');
-    //     // }
-    // });
+    });
+    socket.on('unsubscribe', (topic) => {
+        console.log("UNSUB TOPIC : " + topic);
+        client.unsubscribe(topic);
+    });
+    socket.on('disconnect', ( topic ) => {
+        socketsTopics[socket.id].forEach((topic) => {
+            client.unsubscribe(topic);
+            console.log("DISCON TOPIC : " + topic);
+        });
+        // Remover o socket da lista de sockets
+        delete socketsTopics[socket.id];
+    });
+    socket.on('leave', (topic) => {
+        console.log("LEAVE TOPIC : " + topic);
+        client.unsubscribe(topic);
+    });
 });
 
 server.listen(3001, () => {
@@ -53,11 +76,10 @@ const mqttOptions = {
 
 const client = mqtt.connect(mqttOptions);
 
-client.subscribe('equipamento1/estado')
-client.subscribe('equipamento2/estado')
+// client.subscribe('equipamento/FND001/estado')
+// client.subscribe('equipamento/FND002/estado')
 
 client.on('message', (topic, message) => {
-    console.log(message.toString());
+    console.log(topic, 'VALOR =>' ,message.toString());
     io.emit(topic, message.toString());
-    console.log(topic);
 });
